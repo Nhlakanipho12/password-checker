43dbe61915e4699a1880dcd0af20227c1e1b41a4
keanubowker8 <keanu.bowker@umuzi.org>
2019-11-08 17:44:03 +0200

    complete work

--- spec/password_is_valid.spec.js
+++ spec/password_is_valid.spec.js
@@ -1,8 +1,37 @@
-const passwordIsValid = require("../src/passwordIsValid");
+const {passwordIsValid,passwordIsOk} = require("../src/passwordIsValid");
+const isValid = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/);
 
 describe("passwordIsValid", () => {
-    it("check if password exists", () => {
-        expect(() =>passwordIsValid.verfy(null).toThrow(Error("password cannot be null")))
+    /*password-should exits */
+    it("should check if the password exist", () => {
+        expect(passwordIsValid("P@$$w0rD2")).not.toBeNull();
     });
-    
+    /*password-should have min of 8 char */
+    it("should check if the password has a minimum of 8 characters.", () => {
+        expect(passwordIsValid("P@$$w0rD2").length).toBeGreaterThan(7);
+    });
+
+
+    /*password-should have lowercase char */
+    it("should check if the password has at least one lowercase letter.", () => {
+        expect(passwordIsValid("P@$$w0rD")).toMatch(/^(?=.*[a-z])/);
+    });
+    /*password-should have uppercase char */
+    it("should check if the password has at least one uppercase letter", () => {
+        expect(passwordIsValid("P@$$w0rD")).toMatch(/^(?=.*[A-Z])/);
+    });
+    /*password-should have digit */
+    it("should check if the password has at least one digit.", () => {
+        expect(passwordIsValid("P@$$w0rD")).toMatch(/^(?=.*[0-9])/);
+    })
+    /*password-should have special char */
+    it("should check if the password has at least one special character.", () => {
+        expect(passwordIsValid("P@$$w0rD")).toMatch(/^(?=.*[!@#\$%\^&])/);
+    })
+});
+
+describe("passwordIsOk", () => {
+    it("should check if the given password meets at least three of the conditions listed above then this function should return true, otherwise it should return false.", () => {
+        expect(passwordIsOk("P@$$w0rD")).not.toBeNull();
+    });
 });
--- src/passwordIsValid.js
+++ src/passwordIsValid.js
@@ -1,9 +1,38 @@
-class passwordIsValid{
-    verfy(password){
-        if (password == null){
-            throw Error("Passowrd cannot be null");
+const isUpperCase = /^(?=.*[A-Z])/;
+const isLowerCase = /^(?=.*[a-z])/;
+const hasDigit = /^(?=.*[0-9])/;
+const hasChar = /^(?=.*[!@#$%^&*~])/;
+const isValid = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/);
 
-        }
-    }
+let passwordIsValid =  (password) => { 
 
+    if(password == "") throw new Error("Your function is passed with parameters,yet empty");
+
+    if(password.length < 8) throw new Error("Please make sure your password has a minimum of 8 characters");
+
+    if(password.match(isLowerCase) == null) throw new Error("Your password should have at least one lowercase letter");
+
+    if(password.match(isUpperCase) == null) throw new Error("password should have at least one uppercase letter");
+
+    if(password.match(hasDigit) == null) throw new Error("Your password should at least have one digit");
+
+    if(password.match(hasChar) == null) throw new Error("Your password should have at least one special character");
+
+    return password;
 }
+
+ 
+let passwordIsOk = (password) => {
+    if(password.match(isValid)){
+        
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+ module.exports = {passwordIsValid,passwordIsOk}; 
+ console.log(passwordIsOk("P@$$w0rD"));
+ console.log(passwordIsValid("P@$$w0rD"));
+ 
